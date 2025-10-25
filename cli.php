e2f4ea45989c0ed82726e668e47d20a0162b038d
Clark Joy Pesco <clarkjoypesco@gmail.com>
2025-10-25 16:04:10 +0800

    Fixed courses

--- cli.php
+++ cli.php
@@ -65,15 +65,14 @@ try {
     }
 
     echo "Schema created successfully!\n";
-
-    // Insert courses
     $courses = [
         ['bsit', 'Bachelor of Science in Information Technology', 'Prepares student to be IT professionals who are able to perform installation, operation, development, maintenance, and administration of computer applications'],
         ['bsn', 'Bachelor of Science in Nursing', 'Includes the assessment of patient health problems, developing and implementing nursing care plans, and maintaining medical records. They also administer nursing care to the ill, injured, convalescent, or disabled patients'],
         ['laed', 'Liberal Arts in Education', "Refers to college studies that provide general knowledge and develop intellectual ability. This type of education can prepare you for many fields in today's workplace."],
         ['bsba', 'Bachelor of Science in Business Administration', "Degree program in General Management is a 120-credit program designed for students interested in having exposure to a number of business areas rather than focusing in depth on just one."],
         ['bsa', 'Bachelor of Science in Accountancy', "Is composed of subjects in accounting ( financial, public, managerial), audit, administration, business laws and taxation."],
-        ['fpst', 'Food Preparation & Service Technology', "Is a 2 years degree program which deals with the scientific preparation, processing and distribution of foods. It is also concerned with the improvement of food productsâ€™ flavor, appearance, storage qualities as well as in the control of quality changes during processing, marketing and distribution."]
+        ['fpst', 'Food Preparation & Service Technology', "Is a 2 years degree program which deals with the scientific preparation, processing and distribution of foods. It is also concerned with the improvement of food products’ flavor, appearance, storage qualities as well as in the control of quality changes during processing, marketing and distribution."]
+
     ];
 
     echo "Inserting courses...\n";
